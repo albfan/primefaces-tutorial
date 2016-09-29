4cf06be056e51e1b15a15c0295e16e7686316c6b
albfan <albertofanjul@gmail.com>
2016-09-29 12:47:55 +0200

    Crear un calibre en primefaces

---
+++ src/primefaces/GaugeBean.java
@@ -0,0 +1,31 @@
+package primefaces;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.primefaces.model.chart.MeterGaugeChartModel;
+
+public class GaugeBean implements Serializable {
+
+	private MeterGaugeChartModel meterGaugeModel;
+
+	 public MeterGaugeChartModel getMeterGaugeModel() {
+	  meterGaugeModel.setValue(Math.random() * 220);
+	  return meterGaugeModel;
+	 }
+	 public GaugeBean() {
+	  createMeterGaugeModel();
+	 }
+	 private void createMeterGaugeModel() {
+	  List<Number> intervals = new ArrayList<Number>() {
+	   {
+	    add(20);
+	    add(50);
+	    add(120);
+	    add(220);
+	   }
+	  };
+	  meterGaugeModel = new MeterGaugeChartModel(0, intervals);
+	 }
+}
